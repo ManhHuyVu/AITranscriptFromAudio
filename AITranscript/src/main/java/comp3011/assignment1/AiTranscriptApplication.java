5d305848abb5d1cc8ba3a3128ac26dc7f739bc03
ManhHuyVu <manhhuy.vu@student.adelaide.edu.au>
2026-08-30 12:55:21 +0930

    Setting up service to communicate by REST

--- AITranscript/src/main/java/comp3011/assignment1/AiTranscriptApplication.java
+++ AITranscript/src/main/java/comp3011/assignment1/AiTranscriptApplication.java
@@ -3,6 +3,7 @@ package comp3011.assignment1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AiTranscriptApplication {
 
---
+++ AITranscript/src/main/java/comp3011/assignment1/dto/TranscriptionResponse.java
@@ -0,0 +1,4 @@
+package comp3011.assignment1.dto;
+
+public record TranscriptionResponse(String transcript) {
+}
